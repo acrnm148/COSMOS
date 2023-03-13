0e78e85b74a91fae99e9bc6fc3431d09293c9504
Yun <indl1670@gmail.com>
2023-03-13 16:44:31 +0900

    #S08P22E104-180 #feat:feature/front/search:[장소 검색 페이지 UI 구현 (진행중)]

--- front/src/pages/search/PlaceSearch.tsx
+++ front/src/pages/search/PlaceSearch.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 import SearchIcon from "../../assets/search/SearchIcon.png";
+import "../../css/placeSearch.css";
 
 export default function PlaceSearch() {
   return (
-    <div className="text-center">
+    <div className="text-center w-[80%] max-w-[950px]">
       <h1>장소검색</h1>
-      <div>
-        <select name="" id="">
-          <option value="">시/도</option>
-          <option value="">구/군</option>
-          <option value="">동</option>
+      <div className="flex flex-row">
+        <select className="basis-1/6 border-4 border-lightMain opacity-50 rounded-lg rounded-r-none focus:outline-none">
+          <option className="options" value="">
+            시/도
+          </option>
+          <option className="options" value="">
+            구/군
+          </option>
+          <option className="options" value="">
+            동
+          </option>
         </select>
-        <div className="bg-black">
-          <img src={SearchIcon} alt="" />
-        </div>
+        <input
+          className="basis-4/6 border-4 border-lightMain opacity-50 rounded-lg rounded-l-none focus:outline-none"
+          type="text"
+          placeholder="장소명으로 검색"
+        />
+        <button className="flex flex-col justify-center items-center ml-4 bg-lightMain p-2 rounded-xl">
+          <img className="w-[32px]" src={SearchIcon} alt="검색" />
+        </button>
       </div>
     </div>
   );
