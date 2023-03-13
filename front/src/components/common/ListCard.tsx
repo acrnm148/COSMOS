44a5c63e3c667a34e49f02967787e605a8b33452
Yun <indl1670@gmail.com>
2023-03-13 17:57:08 +0900

    #S08P22E104-180 #feat:feature/front/search:[장소 검색 페이지 UI 구현 (진행중)]

---
+++ front/src/components/common/ListCard.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../../css/listCard.css";
+
+export default function ListCard({ children }: any) {
+  return <div className="card">{children}</div>;
+}
---
+++ front/src/components/common/TMap.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TMap() {
+  return <div className="w-full h-[50vh] bg-black"></div>;
+}
