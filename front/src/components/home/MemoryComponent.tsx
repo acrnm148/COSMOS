0a6a338f0fb63b81b7f1526aec699ae33b6eb90d
dbwls387 <dbwls387@naver.com>
2023-03-14 16:09:42 +0900

    Add: Memory scrollbar hide

--- front/src/components/home/MemoryComponent.tsx
+++ front/src/components/home/MemoryComponent.tsx
@@ -2,7 +2,7 @@ export default function MemoryComponent() {
   return (
     <div className="pt-10 w-full">
       <div className="title text-xl font-bold">우리의 추억</div>
-      <div className="list bg-lightMain4 w-full h-44 mt-2 p-4 rounded-2xl drop-shadow-lg flex overflow-x-scroll">
+      <div className="list bg-lightMain4 w-full h-44 mt-2 p-4 rounded-2xl drop-shadow-lg flex overflow-x-scroll scrollbar-hide">
         <div className="item w-[135px] h-[135px] bg-neutral-300 my-auto mr-5 flex-none rounded-lg text-center leading-[135px] text-lg font-semibold ">
           최근 여행
         </div>
