521eb0bc93c7961046d56bef1d0ded5906a53517
Yun <indl1670@gmail.com>
2023-03-15 11:32:56 +0900

    #S08P22E104-189 #commit feat:feature/front/search:[TMAP API 기능 구현 (진행중)]

--- front/src/components/common/TMap.tsx
+++ front/src/components/common/TMap.tsx
@@ -2,26 +2,36 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 export default function TMap({ state }: any) {
-  const lat = state.center.lat;
-  const lng = state.center.lng;
+  const [position, setPosition] = useState(state);
 
   useEffect(() => {
-    // Tmapv2 사용을 위해 script Element 생성
-    const script = document.createElement("script");
-    script.setAttribute("id", "TMAP");
+    setPosition(state);
+  }, [state]);
 
-    // sds
+  useEffect(() => {
+    console.log(document.head);
+    // 현재 좌표 위치
+    const lat = position.center.lat;
+    const lng = position.center.lng;
+
+    // Tmapv2 사용을 위해 script Element 생성
     const scriptList = document.querySelectorAll("script[id='TMAP']");
-    console.log(scriptList.length);
-    if (scriptList.length >= 0) {
-      console.log(script.parentNode);
-      script.parentNode?.removeChild(script);
-    }
-    // 이후 여기에다가 Tmap 관련 코드 작성
-    script.innerHTML = `
+
+    if (scriptList.length > 0) {
+      const test = document.getElementById("TMAP");
+      console.log(test?.parentNode);
+      for (var i = 0; i < scriptList.length; i++) {
+        scriptList[i].parentNode?.removeChild(scriptList[i]);
+      }
+      test?.parentNode?.removeChild(test);
+    } else {
+      const script = document.createElement("script");
+      script.setAttribute("id", "TMAP");
+      // 이후 여기에다가 Tmap 관련 코드 작성 d
+      script.innerHTML = `
         function initTmap() {
             var map = new Tmapv2.Map("TMapApp", {
-                center: new Tmapv2.LatLng("${lat}", "${lng}"),
+                center: new Tmapv2.LatLng(${lat}, ${lng}),
                 width: "100%",
                 height: "50vh",
                 zoom:15
@@ -29,14 +39,31 @@ export default function TMap({ state }: any) {
         }
 
         initTmap();
-   `;
-    if (lat !== 0 && lng !== 0) {
-      // 자바스크립트 형식으로 설정 후 document에 추가
-      script.type = "text/javascript";
-      script.async = true;
-
-      document.head.appendChild(script);
+        `;
+      if (lat !== 0 && lng !== 0) {
+        // 자바스크립트 형식으로 설정 후 document에 추가
+        script.type = "text/javascript";
+        script.async = true;
+        console.log(script);
+        document.head.appendChild(script);
+      }
     }
-  }, [state]);
-  return <div className="w-full h-[50vh]" id="TMapApp"></div>;
+  }, [position]);
+  return (
+    <>
+      <div className="w-full h-[50vh]" id="TMapApp"></div>
+      <button
+        onClick={() =>
+          setPosition({
+            center: {
+              lat: 37.5652045,
+              lng: 126.98702028,
+            },
+          })
+        }
+      >
+        클릭
+      </button>
+    </>
+  );
 }
