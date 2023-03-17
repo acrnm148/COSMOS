4faee1b3f182ea153720e341c05e7ad9193aafe3
Yun <indl1670@gmail.com>
2023-03-17 17:49:01 +0900

    chore: 지도 UI css 수정

--- front/src/components/common/Map/KoreaMap.tsx
+++ front/src/components/common/Map/KoreaMap.tsx
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { getRegionList } from "./parsingRegionData";
 import SouthKoreaItem from "./KoreaItem";
 import { useSetRecoilState } from "recoil";
 import { selectSidoState } from "../../../recoil/states/RecommendPageState";
 import "../../../css/map.css";
-export default function SouthKorea({ list }: any) {
+export default function SouthKorea({ list, setSelectSido }: any) {
+  const [selSido, setSelSido] = useState("");
   const setSido = useSetRecoilState(selectSidoState);
 
   return (
     <div className="map-wrapper">
       <div className="info"></div>
       <svg
-        width="400"
-        height="800"
+        width="375"
+        height="700"
         viewBox="30 100 400 800"
         xmlns="http://www.w3.org/2000/svg"
       >
         <g>
           {getRegionList(list).map((item: any) => {
-            console.log(item);
             return (
               <SouthKoreaItem
                 onClick={() => {
-                  setSido({ sidoCode: item.sidoCode, name: item.sidoName });
+                  setSido({ sidoCode: item.sidoCode, name: item.name });
+                  setSelSido(item.name);
+                  setSelectSido(item.name);
                 }}
                 key={item.sidoCode}
-                className={`land`}
+                className={selSido === item.name ? "land click" : "land"}
                 title={item.name}
                 d={item.d}
               />
