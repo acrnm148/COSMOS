b8b067e983a538d429416d0de7d93cea04892fd3
beetrootfarmer <pos04118@gmail.com>
2023-03-18 14:41:35 +0900

    S08P22E104-241 dragndrop

--- front/src/components/common/PlaceItem.tsx
+++ front/src/components/common/PlaceItem.tsx
@@ -13,7 +13,9 @@ export function PlaceItem(props:{place:Place}){
     })
     return (
         <div className="flex w-full h-40 bg-calendarGray mt-2 rounded-lg justify-between p-2">
-            <div className="w-2/6 h-full bg-calendarDark rounded-lg">이미지</div>
+            <div className='h-full bg-calendarDark rounded-lg overflow-hidden'>
+                <img src={plc.imgUrl} alt="" />
+            </div>
             <div className="flex flex-col w-4/6">
                 <div className="w-4/6 flex justify-start ml-2">
                     <div>{plc.location}</div>
--- front/src/pages/Schedule/DaySchedulePage.tsx
+++ front/src/pages/Schedule/DaySchedulePage.tsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { PlaceItem } from "../../components/common/PlaceItem";
 import { ScheduleMonth } from "../../components/common/ScheduleMonth";
 import { WeekCalendar } from "../../components/common/WeekCalendar";
+
+// 지울예정
+import 파주출판단지 from "../../assets/schedule/파주출판단지.png"
+import 녹두 from "../../assets/schedule/녹두.png"
+import 베지앙 from "../../assets/schedule/베지앙.png"
 
 interface Place{
     idx : number,
@@ -13,12 +18,47 @@ interface Place{
     date: string,
 }
 
-const testPlace:Place[] = [{idx:0,name:'파주 출판단지', imgUrl:'', category:'관광', location:"경기도", date:'2023년 2월 28일'},
-                        {idx:1,name:'베지앙', imgUrl:'', category:'카페', location:"경기도", date:'2023년 2월 28일'},
-                        {idx:2,name:'녹두', imgUrl:'', category:'음식', location:"경기도", date:'2023년 2월 28일'}
+const testPlace:Place[] = [{idx:0,name:'파주 출판단지', imgUrl:파주출판단지, category:'관광', location:"경기도", date:'2023년 2월 28일'},
+                        {idx:1,name:'베지앙', imgUrl:베지앙, category:'카페', location:"경기도", date:'2023년 2월 28일'},
+                        {idx:2,name:'녹두', imgUrl:녹두, category:'음식', location:"경기도", date:'2023년 2월 28일'}
 ]
 export function DaySchedulePage(){
     const [scheduleTitle, setScheduleTitle] = useState('파릇파릇파주여행')
+    const [places, setPlaces] = useState<Place[]>([])
+    // 일정 정보 초기화
+    useEffect(()=>{
+        setPlaces([...testPlace])
+    },[])
+
+    // 드래그앤드랍
+    const draggingIdx = useRef<null | number>(null)
+    const draggingOverIdx = useRef<null | number>(null)
+    // 드래그 시작 (아이템 확인)
+    const onDragStart = (e: any, idx: number) =>{
+        draggingIdx.current = idx
+        e.target.classList.add('grabbing')
+    }
+    // 드래그 놓았을 때 변화한 리스트 적용
+    const onDragEnter = (e: any, idx: number) => {
+        draggingOverIdx.current = idx
+        const copyList = [...places]
+        const dragItemContent = copyList[draggingIdx.current!]
+        copyList.splice(draggingIdx.current!, 1)
+        copyList.splice(draggingOverIdx.current, 0, dragItemContent)
+        draggingIdx.current = draggingOverIdx.current
+        draggingOverIdx.current = null
+        setPlaces(copyList)
+        e.target.classList.remove('grabbing')
+    }
+    // 드래그 이동 종료
+    const onDragEnd = (e: any) =>{
+        e.target.classList.remove('grabbing')
+    }
+    // 드래그 하는 중 아이템이 오버랩 된 상태
+    const onDragOver = (e: { preventDefault: () => void; }) =>{
+        e.preventDefault()
+    }
+
     return (
         <div className="bg-lightMain2 h-screen">
             <ScheduleMonth />
@@ -29,11 +69,21 @@ export function DaySchedulePage(){
                         <div className="">
                             <ScheduleTitle scheduleTitle={scheduleTitle}/>
                             {
-                                testPlace.map((place, key)=>{
+                                places?.map((place, key)=>{
                                     return(
-                                        <NavLink to="/schedule/detail" state={{placeId: key, place:place, scheduleTitle:scheduleTitle}}>
-                                            <PlaceItem place={place} key={key}/>
-                                        </NavLink>
+                                        <div
+                                            // onClick={} 페이지이동
+                                            onDragStart={(e) => onDragStart(e, key)}
+                                            onDragEnter={(e) => onDragEnter(e, key)}
+                                            onDragOver={onDragOver}
+                                            onDragEnd={onDragEnd}
+                                            draggable
+                                            key={key}
+                                        >
+                                            {/* <NavLink to="/schedule/detail" state={{placeId: key, place:place, scheduleTitle:scheduleTitle}}> */}
+                                                <PlaceItem place={place} key={key}/>
+                                            {/* </NavLink> */}
+                                        </div>
                                     ) 
                                 })
                             }
