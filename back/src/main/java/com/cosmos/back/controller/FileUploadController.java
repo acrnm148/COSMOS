83a83c465634dd3a115e9743011c807b0763bebe
강수나 <acrnm148@gmail.com>
2023-03-20 05:33:34 +0900

    Add: 다중파일업로드

--- back/src/main/java/com/cosmos/back/controller/FileUploadController.java
+++ back/src/main/java/com/cosmos/back/controller/FileUploadController.java
@@ -1,42 +1,27 @@
 package com.cosmos.back.controller;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.cosmos.back.service.S3Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
+@Tag(name = "s3 file upload", description = "파일 업로드 테스트")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class FileUploadController {
 
-    private final AmazonS3Client amazonS3Client;
+    private final S3Service s3Service;
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        try {
-            String fileName=file.getOriginalFilename();
-            String fileUrl= "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" +fileName;
-            ObjectMetadata metadata= new ObjectMetadata();
-            metadata.setContentType(file.getContentType());
-            metadata.setContentLength(file.getSize());
-            amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
-            return ResponseEntity.ok(fileUrl);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+    @Operation(summary = "파일 업로드 테스트", description = "파일 업로드 테스트")
+    @PostMapping("/file")
+    public ResponseEntity<List<String>> uploadFile(@RequestPart("file") List<MultipartFile> multipartFile) {
+        return ResponseEntity.ok(s3Service.uploadFiles(multipartFile));
     }
 }
---
+++ back/src/main/java/com/cosmos/back/service/S3Service.java
@@ -0,0 +1,61 @@
+package com.cosmos.back.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    /**
+     * 다중 파일 업로드
+     */
+    public List<String> uploadFiles(List<MultipartFile> multipartFile) {
+        List<String> fileNameList = new ArrayList<>();
+
+        // forEach 구문을 통해 multipartFile로 넘어온 파일들 하나씩 fileNameList에 추가
+        multipartFile.forEach(file -> {
+            try {
+                String fileName = createFileName(file.getOriginalFilename());
+                String fileUrl= "https://" + bucket + ".s3.ap-northeast-2.amazonaws.com/" +fileName;
+                ObjectMetadata objectMetadata = new ObjectMetadata();
+                objectMetadata.setContentType(file.getContentType());
+                objectMetadata.setContentLength(file.getSize());
+
+                amazonS3Client.putObject(bucket, fileName, file.getInputStream(), objectMetadata);
+                fileNameList.add(fileUrl);
+            } catch(IOException e) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
+            }
+        });
+
+        return fileNameList;
+    }
+
+    private String createFileName(String fileName) { // 먼저 파일 업로드 시, 파일명을 난수화하기 위해 random으로 돌립니다.
+        UUID uuid = UUID.randomUUID();
+        String[] splitString = fileName.split("\\.");
+        String ext = splitString[splitString.length -1];
+        String changedFileName = uuid.toString() +"."+ext;
+
+        return changedFileName;
+    }
+
+}
