47c9db47e3d164e1140f2ea521bf92bdf637707b
dbwls387 <dbwls387@naver.com>
2023-03-20 08:20:04 +0900

    Add: WishLIst

---
+++ front/src/components/wish/WishCourse.tsx
@@ -0,0 +1,3 @@
+export default function WishCourse() {
+  return <div>코스리스트~~</div>;
+}
---
+++ front/src/components/wish/WishPlace.tsx
@@ -0,0 +1,7 @@
+export default function WishPlace() {
+  return (
+    <div>
+      <div className="">장소리스트트트트트트트트트로트</div>
+    </div>
+  );
+}
