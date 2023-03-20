14d7eaf01287690a082a10629f63d2f362c90aed
dbwls387 <dbwls387@naver.com>
2023-03-20 17:58:42 +0900

    Add: WishList-place

--- front/src/components/wish/WishList.tsx
+++ front/src/components/wish/WishList.tsx
@@ -3,19 +3,37 @@ import WishCourse from "../wish/WishCourse";
 import { useState } from "react";
 
 export default function WishList() {
-  const [toggle, setToggle] = useState(true); // 장소: true / 코스: false
+    const [toggle, setToggle] = useState(true); // 장소: true / 코스: false
 
-  return (
-    <div>
-      <div className="menu w-full h-16 flex items-center">
-        <div className="placeBtn w-1/2 h-full pt-[18px] float-left text-center text-xl border-b-4 border-lightMain">
-          장소
+    return (
+        <div>
+            <div className="menu w-full h-16 flex items-center">
+                <div
+                    className={
+                        "placeBtn w-1/2 h-full pt-[18px] float-left text-center text-xl" +
+                        (toggle ? " font-bold border-b-4 border-lightMain" : "")
+                    }
+                    onClick={() => {
+                        setToggle(true);
+                    }}
+                >
+                    장소
+                </div>
+                <div
+                    className={
+                        "courseBtn w-1/2 h-full pt-[18px] float-left text-center text-xl" +
+                        (!toggle ? " font-bold border-b-4 border-lightMain" : "")
+                    }
+                    onClick={() => {
+                        setToggle(false);
+                    }}
+                >
+                    코스
+                </div>
+            </div>
+
+            {toggle && <WishPlace />}
+            {!toggle && <WishCourse />}
         </div>
-        <div className="courseBtn w-1/2 h-full pt-[18px] float-left text-center text-xl">코스</div>
-      </div>
-
-      {toggle && <WishPlace />}
-      {!toggle && <WishCourse />}
-    </div>
-  );
+    );
 }
