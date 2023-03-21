b2653fc7ffe5cb5e00bf1ba8630f8542b43c3918
Yun <indl1670@gmail.com>
2023-03-21 12:10:03 +0900

    #S08P22E104-281 #commit feat:feature/front/recommend:[장소 추천 페이지 세부 기능 구현 - 코스 카테고리 선택 기능 구현]

--- front/src/components/recommend/SelectCategory.tsx
+++ front/src/components/recommend/SelectCategory.tsx
@@ -1,17 +1,89 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import SelectCategoryItem from "./SelectCategoryItem";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import Logo from "../../assets/login/pinkCosmos.png";
 
 export default function SelectCategory() {
+  const navigate = useNavigate();
   const categoryRef = useRef<HTMLDivElement>(null);
   const [number, setNumber] = useState(1);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([{ index: 0, state: true }]);
   const tags = [false, false, false, false, false, false];
+  const [selectedList, setSelectedList] = useState([]);
+
   const handleClickBtn = () => {
     setNumber((cur) => cur + 1);
+    setList(list.concat({ index: number, state: true }));
   };
 
-  useEffect(() => {}, [number]);
+  const handleComplete = () => {
+    const clicked = document.querySelectorAll(".content-clicked");
+    const selected = [];
+    for (let i = 0; i < clicked.length; i++) {
+      switch (clicked[i].className[16]) {
+        case "0":
+          selected.push("f");
+          break;
+        case "1":
+          selected.push("c");
+          break;
+        case "2":
+          selected.push("m");
+          break;
+        case "3":
+          selected.push("s");
+          break;
+        case "4":
+          selected.push("t");
+          break;
+        case "5":
+          selected.push("g");
+          break;
+        default:
+      }
+    }
+
+    // setSelectedList(selected);
+
+    Swal.fire({
+      title: "카테고리 선택 완료!",
+      text: "코스 추천 페이지로 이동합니다.",
+      icon: "success",
+
+      showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+      confirmButtonColor: "#FF8E9E", // confrim 버튼 색깔 지정
+      cancelButtonColor: "#A3A3A3", // cancel 버튼 색깔 지정
+      confirmButtonText: "승인", // confirm 버튼 텍스트 지정
+      cancelButtonText: "취소", // cancel 버튼 텍스트 지정
+
+      reverseButtons: false, // 버튼 순서 거꾸로
+    }).then((result) => {
+      // 만약 Promise리턴을 받으면,
+      if (result.isConfirmed) {
+        // 만약 모달창에서 confirm 버튼을 눌렀다면
+
+        Swal.fire({
+          imageUrl: Logo,
+          imageWidth: 200,
+          imageHeight: 200,
+          imageAlt: "대체 이미지",
+          title: "L O A D I N G . . .",
+          timer: 3000,
+          timerProgressBar: true,
+          showConfirmButton: false,
+          didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+          },
+        }).then(() => {
+          // 추천 코스 페이지로 이동
+          navigate("/");
+        });
+      }
+    });
+  };
 
   return (
     <div className="mb-[90px]">
@@ -23,8 +95,16 @@ export default function SelectCategory() {
         <AddCircleIcon sx={{ color: "#FF8E9E", fontSize: 40 }} />
       </div>
       <div ref={categoryRef}>
-        <SelectCategoryItem number={number} tags={tags} />
+        {list.map((item) => {
+          return <SelectCategoryItem tags={tags} key={item.index} />;
+        })}
       </div>
+      <button
+        className="float-right mr-[10px] text-white bg-lightMain p-[10px] rounded-lg hover:bg-lightMain2"
+        onClick={handleComplete}
+      >
+        선택 완료
+      </button>
     </div>
   );
 }
--- front/src/components/recommend/SelectCategoryItem.tsx
+++ front/src/components/recommend/SelectCategoryItem.tsx
@@ -5,79 +5,83 @@ import Coffee from "../../assets/place/coffee-cup.png";
 import Shopping from "../../assets/place/shopping-cart.png";
 import Gym from "../../assets/place/gym.png";
 import Suitcase from "../../assets/place/suitcase.png";
+import LoveIcon from "../../assets/place/love-icon.png";
 
-export default function SelectCategoryItem({ number, tags }: any) {
-  const num = number;
+export default function SelectCategoryItem({ tags }: any) {
   const [tag, setTag] = useState(tags);
 
   return (
-    <div className="modal-content first-child">
-      <span className="category-number">{num}</span>
-      <div
-        className={tag[0] ? "content-clicked" : ""}
-        onClick={() => setTag([true, false, false, false, false, false])}
-      >
-        <img
-          src={Cutlery}
-          alt=""
-          style={tag[0] ? { opacity: 1 } : { opacity: 0.3 }}
-        />
-        <p>음식</p>
-      </div>
-      <div
-        className={tag[1] ? "content-clicked" : ""}
-        onClick={() => setTag([false, true, false, false, false, false])}
-      >
-        <img
-          src={Coffee}
-          alt=""
-          style={tag[1] ? { opacity: 1 } : { opacity: 0.3 }}
-        />
-        <p>카페</p>
-      </div>
-      <div
-        className={tag[2] ? "content-clicked" : ""}
-        onClick={() => setTag([false, false, true, false, false, false])}
-      >
-        <img
-          src={Cinema}
-          alt=""
-          style={tag[2] ? { opacity: 1 } : { opacity: 0.3 }}
-        />
-        <p>문화</p>
-      </div>
-      <div
-        className={tag[3] ? "content-clicked" : ""}
-        onClick={() => setTag([false, false, false, true, false, false])}
-      >
-        <img
-          src={Shopping}
-          alt=""
-          style={tag[3] ? { opacity: 1 } : { opacity: 0.3 }}
-        />
-        <p>쇼핑</p>
-      </div>
-      <div
-        className={tag[4] ? "content-clicked" : ""}
-        onClick={() => setTag([false, false, false, false, true, false])}
-      >
-        <img
-          src={Suitcase}
-          alt=""
-          style={tag[4] ? { opacity: 1 } : { opacity: 0.3 }}
-        />
-        <p>관광</p>
-      </div>
-      <div
-        className={tag[5] ? "content-clicked" : ""}
-        onClick={() => setTag([false, false, false, false, false, true])}
-      >
-        <img
-          src={Gym}
-          alt=""
-          style={tag[5] ? { opacity: 1 } : { opacity: 0.3 }}
-        />
-        <p>운동</p>
+    <div className="flex flex-row justify-center gap-[5vw]">
+      <span className="icon-span">
+        <img className="love-icon" src={LoveIcon} alt="" />
+      </span>
+      <div className="modal-content">
+        <div
+          className={tag[0] ? "content-clicked 0" : ""}
+          onClick={() => setTag([true, false, false, false, false, false])}
+        >
+          <img
+            src={Cutlery}
+            alt=""
+            style={tag[0] ? { opacity: 1 } : { opacity: 0.3 }}
+          />
+          <p>음식</p>
+        </div>
+        <div
+          className={tag[1] ? "content-clicked 1" : ""}
+          onClick={() => setTag([false, true, false, false, false, false])}
+        >
+          <img
+            src={Coffee}
+            alt=""
+            style={tag[1] ? { opacity: 1 } : { opacity: 0.3 }}
+          />
+          <p>카페</p>
+        </div>
+        <div
+          className={tag[2] ? "content-clicked 2" : ""}
+          onClick={() => setTag([false, false, true, false, false, false])}
+        >
+          <img
+            src={Cinema}
+            alt=""
+            style={tag[2] ? { opacity: 1 } : { opacity: 0.3 }}
+          />
+          <p>문화</p>
+        </div>
+        <div
+          className={tag[3] ? "content-clicked 3" : ""}
+          onClick={() => setTag([false, false, false, true, false, false])}
+        >
+          <img
+            src={Shopping}
+            alt=""
+            style={tag[3] ? { opacity: 1 } : { opacity: 0.3 }}
+          />
+          <p>쇼핑</p>
+        </div>
+        <div
+          className={tag[4] ? "content-clicked 4" : ""}
+          onClick={() => setTag([false, false, false, false, true, false])}
+        >
+          <img
+            src={Suitcase}
+            alt=""
+            style={tag[4] ? { opacity: 1 } : { opacity: 0.3 }}
+          />
+          <p>관광</p>
+        </div>
+        <div
+          className={tag[5] ? "content-clicked 5" : ""}
+          onClick={() => setTag([false, false, false, false, false, true])}
+        >
+          <img
+            src={Gym}
+            alt=""
+            style={tag[5] ? { opacity: 1 } : { opacity: 0.3 }}
+          />
+          <p>운동</p>
+        </div>
       </div>
     </div>
   );
