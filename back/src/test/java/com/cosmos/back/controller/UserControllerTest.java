7bb8d2705865d32e9e489af4e15f95191aaad0af
강수나 <acrnm148@gmail.com>
2023-03-22 09:41:54 +0900

    Add : test

---
+++ back/src/test/java/com/cosmos/back/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package com.cosmos.back.controller;
+
+import com.cosmos.back.annotation.EnableMockMvc;
+import net.bytebuddy.asm.Advice;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@EnableMockMvc
+public class UserControllerTest {
+
+
+}
