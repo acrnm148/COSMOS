060ec71044af92f1af15b80dee9cbe5d190d6ce0
SIU-JANG <qq6988@naver.com>
2023-03-23 11:43:03 +0900

    Add : redis

--- back/src/main/java/com/cosmos/back/aspect/RedisCacheAspect.java
+++ back/src/main/java/com/cosmos/back/aspect/RedisCacheAspect.java
@@ -61,7 +61,6 @@ public class RedisCacheAspect {
                 }
             }
 
-            System.out.println(methodInfo);
             cacheParameterMethodInfoMap.put(key, methodInfo);
         }
 
