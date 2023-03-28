761423d37f74f44021b9630b9f53fbfa5f1b87c3
dbwls387 <dbwls387@naver.com>
2023-03-28 14:05:54 +0900

    Add: wish place api

--- front/src/apis/api/wish.ts
+++ front/src/apis/api/wish.ts
@@ -2,16 +2,7 @@ import { AxiosAuthApi, defaultInstance } from "../utils";
 
 // GET APIs
 
-/**
- * @param {number} userSeq : 유저 seq
- * GET : 찜한 장소 리스트 데이터를 가져온다.
- * @returns [] : 찜한 장소 id, name, score, address, detail, thumbNailUrl
- */
-export const getWishPlaceList = async (userSeq: number) => {
-    const { data } = await defaultInstance.get(`places/users/${userSeq}`);
-    return data;
-};
-
+// 코스
 /**
  * @param {number} userSeq : 유저 seq
  * GET : 찜한 코스 리스트 데이터를 가져온다.
@@ -23,6 +14,18 @@ export const getWishCourseList = async (userSeq: number, ac: string | null) => {
     return data;
 };
 
+// 장소
+/**
+ * @param {number} userSeq : 유저 seq
+ * GET : 찜한 장소 리스트 데이터를 가져온다.
+ * @returns [] : 찜한 장소 id, name, score, address, detail, thumbNailUrl
+ */
+export const getWishPlaceList = async (userSeq: number) => {
+    const { data } = await defaultInstance.get(`places/users/${userSeq}`);
+    return data;
+};
+
+// DELETE APIs
 /**
  * @param {number} courseId : 코스 id
  * DELETE : 찜한 코스를 찜 목록에서 삭제한다.
--- front/src/components/wish/WishPlace.tsx
+++ front/src/components/wish/WishPlace.tsx
@@ -5,25 +5,139 @@ import { getWishPlaceList } from "../../apis/api/wish";
 import { useQuery } from "react-query";
 import { useRecoilState } from "recoil";
 import { userState } from "../../recoil/states/UserState";
+import { useEffect, useState } from "react";
 
 /* eslint-disable jsx-a11y/alt-text */
-type propsType = {
-    id: number;
-    title: string;
+interface Place {
+    placeId: number;
+    name: string;
+    score: number;
     address: string;
-    content: string;
-    star: number;
-    heart: boolean;
-};
+    detail: string;
+    thumbNailUrl: string;
+}
 
 export default function WishPlace() {
     const userSeq = useRecoilState(userState);
-    // const { data, isLoading } = useQuery({
-    //     queryKey: ["getWishPlaceList", "userSeq"],
-    //     queryFn: () => getWishPlaceList(userSeq[0].seq),
-    // });
+    const [list, setList] = useState<Place[]>();
+    const { data } = useQuery({
+        queryKey: ["getWishPlaceList", "userSeq"],
+        queryFn: () => getWishPlaceList(userSeq[0].seq),
+    });
 
-    let list: propsType[] = [
+    useEffect(() => {
+        setList(data);
+    }, [data]);
+    console.log(data);
+
+    return (
+        <div>
+            <div className="mx-5 mt-5 h-full">
+                <div className="title font-medium text-xl inline-block">
+                    찜한 장소
+                </div>
+                {/* <div className="cnt ml-2 font-bold text-red-600 text-xl inline-block">
+                    {list2.length}
+                </div>
+
+                <div className="mt-4 h-[480px] overflow-y-auto">
+                    {list2.map((a: Place) => (
+                        <Item key={a.id} item={a}></Item>
+                    ))}
+                </div> */}
+
+                <div className="w-full h-12 bg-lightMain2 text-white font-bold text-center text-xl pt-2.5 mt-3 rounded-full">
+                    찜한 장소로 코스 만들기
+                </div>
+            </div>
+        </div>
+    );
+}
+
+// function Item(props: { item: Place }) {
+//     let address =
+//         props.item.address.length > 18
+//             ? props.item.address.slice(0, 18).concat("...")
+//             : props.item.address;
+//     let content =
+//         props.item.content.length > 30
+//             ? props.item.content.slice(0, 30).concat("...")
+//             : props.item.content;
+
+//     return (
+//         <div className="col-md-4 mb-4 p-3 h-40 bg-calendarGray rounded-lg">
+//             <div className="heart mb-2 float-right">
+//                 {props.item.heart && (
+//                     <Icon
+//                         icon="mdi:cards-heart"
+//                         color="#ff8e9e"
+//                         width="28"
+//                         height="28"
+//                         onClick={() => {
+//                             Swal.fire({
+//                                 // title: `${props.item.title}을 찜 해제하시겠습니까?`,
+//                                 text: `${props.item.title}을(를) 찜 해제하시겠습니까?`,
+//                                 icon: "question",
+
+//                                 showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+//                                 confirmButtonColor: "#FF8E9E", // confrim 버튼 색깔 지정
+//                                 cancelButtonColor: "#B9B9B9", // cancel 버튼 색깔 지정
+//                                 confirmButtonText: "승인", // confirm 버튼 텍스트 지정
+//                                 cancelButtonText: "취소", // cancel 버튼 텍스트 지정
+//                             }).then((result) => {
+//                                 // 만약 Promise리턴을 받으면,
+//                                 if (result.isConfirmed) {
+//                                     // 만약 모달창에서 confirm 버튼을 눌렀다면
+//                                     // 해당 장소 찜 목록에서 삭제
+//                                     const Toast = Swal.mixin({
+//                                         toast: true, // 토스트 형식
+//                                         position: "bottom-end", // 알림 위치
+//                                         showConfirmButton: false, // 확인버튼 생성 유무
+//                                         timer: 1000, // 지속 시간
+//                                         timerProgressBar: true, // 지속시간바 생성 여부
+//                                     });
+
+//                                     Toast.fire({
+//                                         icon: "success",
+//                                         title: "삭제되었습니다. ",
+//                                     });
+//                                 }
+//                             });
+//                         }}
+//                     />
+//                 )}
+//                 {!props.item.heart && (
+//                     <Icon
+//                         icon="mdi:cards-heart-outline"
+//                         color="#ff8e9e"
+//                         width="28"
+//                         height="28"
+//                     />
+//                 )}
+//             </div>
+
+//             <div className="font-bold mb-2">{props.item.title}</div>
+
+//             <img
+//                 className="float-left w-24 h-[100px] mr-2 rounded-md"
+//                 src="https://img.siksinhot.com/place/1600741858600366.jpg?w=307&h=300&c=Y"
+//             />
+
+//             <div className="star ml-2 mb-1">
+//                 <Star fontSize="small" sx={{ color: "#FF8E9E" }} />
+//                 <p className="inline-block text-sm ml-1 text-lightMain">
+//                     {props.item.star}
+//                 </p>
+//             </div>
+//             <div className="mb-2 text-sm text-gray-500">{address}</div>
+//             <div className="text-sm">{content}</div>
+//         </div>
+//     );
+// }
+
+/*
+
+let list2: Place[] = [
         {
             id: 0,
             title: "사발",
@@ -51,107 +165,4 @@ export default function WishPlace() {
         },
     ];
 
-    return (
-        <div>
-            <div className="mx-5 mt-5 h-full">
-                <div className="title font-medium text-xl inline-block">
-                    찜한 장소
-                </div>
-                <div className="cnt ml-2 font-bold text-red-600 text-xl inline-block">
-                    {list.length}
-                </div>
-
-                <div className="mt-4 h-[480px] overflow-y-auto">
-                    {list.map((a: propsType) => (
-                        <Item key={a.id} item={a}></Item>
-                    ))}
-                </div>
-
-                <div className="w-full h-12 bg-lightMain2 text-white font-bold text-center text-xl pt-2.5 mt-3 rounded-full">
-                    찜한 장소로 코스 만들기
-                </div>
-            </div>
-        </div>
-    );
-}
-
-function Item(props: { item: propsType }) {
-    let address =
-        props.item.address.length > 18
-            ? props.item.address.slice(0, 18).concat("...")
-            : props.item.address;
-    let content =
-        props.item.content.length > 30
-            ? props.item.content.slice(0, 30).concat("...")
-            : props.item.content;
-
-    return (
-        <div className="col-md-4 mb-4 p-3 h-40 bg-calendarGray rounded-lg">
-            <div className="heart mb-2 float-right">
-                {props.item.heart && (
-                    <Icon
-                        icon="mdi:cards-heart"
-                        color="#ff8e9e"
-                        width="28"
-                        height="28"
-                        onClick={() => {
-                            Swal.fire({
-                                // title: `${props.item.title}을 찜 해제하시겠습니까?`,
-                                text: `${props.item.title}을(를) 찜 해제하시겠습니까?`,
-                                icon: "question",
-
-                                showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
-                                confirmButtonColor: "#FF8E9E", // confrim 버튼 색깔 지정
-                                cancelButtonColor: "#B9B9B9", // cancel 버튼 색깔 지정
-                                confirmButtonText: "승인", // confirm 버튼 텍스트 지정
-                                cancelButtonText: "취소", // cancel 버튼 텍스트 지정
-                            }).then((result) => {
-                                // 만약 Promise리턴을 받으면,
-                                if (result.isConfirmed) {
-                                    // 만약 모달창에서 confirm 버튼을 눌렀다면
-                                    // 해당 장소 찜 목록에서 삭제
-                                    const Toast = Swal.mixin({
-                                        toast: true, // 토스트 형식
-                                        position: "bottom-end", // 알림 위치
-                                        showConfirmButton: false, // 확인버튼 생성 유무
-                                        timer: 1000, // 지속 시간
-                                        timerProgressBar: true, // 지속시간바 생성 여부
-                                    });
-
-                                    Toast.fire({
-                                        icon: "success",
-                                        title: "삭제되었습니다. ",
-                                    });
-                                }
-                            });
-                        }}
-                    />
-                )}
-                {!props.item.heart && (
-                    <Icon
-                        icon="mdi:cards-heart-outline"
-                        color="#ff8e9e"
-                        width="28"
-                        height="28"
-                    />
-                )}
-            </div>
-
-            <div className="font-bold mb-2">{props.item.title}</div>
-
-            <img
-                className="float-left w-24 h-[100px] mr-2 rounded-md"
-                src="https://img.siksinhot.com/place/1600741858600366.jpg?w=307&h=300&c=Y"
-            />
-
-            <div className="star ml-2 mb-1">
-                <Star fontSize="small" sx={{ color: "#FF8E9E" }} />
-                <p className="inline-block text-sm ml-1 text-lightMain">
-                    {props.item.star}
-                </p>
-            </div>
-            <div className="mb-2 text-sm text-gray-500">{address}</div>
-            <div className="text-sm">{content}</div>
-        </div>
-    );
-}
+*/
