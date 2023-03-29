daa92d2f65962fd7e467fbbfedab540cacc7f08e
Yun <indl1670@gmail.com>
2023-03-29 11:45:42 +0900

    #S08P22E104-375 #commit feat:feature/front/search:[장소 검색 페이지 추가 기능 구현 - 시/도, 구/군 선택 기능 구현]

---
+++ front/src/components/place/lists/GugunList.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { getGugunList } from "../../../apis/api/place";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import {
+  selectSido,
+  selectGugun,
+} from "../../../recoil/states/SearchPageState";
+
+export default function GugunList() {
+  // 구/군 state값
+  const sido = useRecoilState(selectSido);
+
+  // 구/군 Recoil 수정
+  const [gugun, setGugun] = useRecoilState(selectGugun);
+
+  // 구/군 선택
+  const handleGugunList = (e: any) => {
+    const idx = e.target.options.selectedIndex;
+    const code = e.target.options[idx].getAttribute("data-key"); // 구군코드
+    const name = e.target.value; // 구군이름
+    const selected = { gugunCode: code, gugunName: name };
+    setGugun(selected);
+  };
+
+  // 구/군 리스트 얻어옴
+  const { data, isLoading } = useQuery({
+    queryKey: ["getGugunList", sido],
+    queryFn: () => getGugunList(sido[0].sidoCode),
+  });
+
+  if (isLoading) return null;
+
+  return (
+    <select
+      className="w-full h-14 m-auto border-[4px] border-lightMain opacity-50 rounded-r-lg outline-none focus:bg-lightMain3"
+      name="selectSido"
+      id="selectSido"
+      onChange={handleGugunList}
+      value={gugun.gugunName === "" ? "구/군 선택" : gugun.gugunName}
+    >
+      <option value="">구/군 선택</option>
+      {data === undefined || data === null
+        ? null
+        : data.map((item: any) => {
+            return (
+              <option
+                value={item.gugunName}
+                key={item.gugunCode}
+                data-key={item.gugunCode}
+              >
+                {item.gugunName}
+              </option>
+            );
+          })}
+    </select>
+  );
+}
---
+++ front/src/components/place/lists/SidoList.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { getSidoList } from "../../../apis/api/place";
+import { useRecoilState } from "recoil";
+import { selectSido } from "../../../recoil/states/SearchPageState";
+export default function SidoList() {
+  // 시/도 Recoil 수정
+  const [sido, setSido] = useRecoilState(selectSido);
+  // 시/도 선택
+  const handleSidoList = (e: any) => {
+    const idx = e.target.options.selectedIndex;
+    const code = e.target.options[idx].getAttribute("data-key"); // 시도코드
+    const name = e.target.value; // 시도이름
+
+    const selected = { sidoCode: code, sidoName: name };
+    setSido(selected);
+  };
+
+  // 시/도 리스트 얻어옴
+  const { data, isLoading } = useQuery({
+    queryKey: ["getSidoList"],
+    queryFn: () => getSidoList(),
+  });
+
+  if (isLoading) return null;
+
+  return (
+    <select
+      className="w-full h-14 m-auto border-[4px] border-lightMain opacity-50 rounded-l-lg outline-none hover:bg-lightMain3"
+      name="selectSido"
+      id="selectSido"
+      onChange={handleSidoList}
+      value={sido.sidoName === "" ? "시/도 선택" : sido.sidoName}
+    >
+      <option value="">시/도 선택</option>
+      {data.map((item: any) => {
+        return (
+          <option
+            value={item.sidoName}
+            key={item.sidoCode}
+            data-key={item.sidoCode}
+          >
+            {item.sidoName}
+          </option>
+        );
+      })}
+    </select>
+  );
+}
