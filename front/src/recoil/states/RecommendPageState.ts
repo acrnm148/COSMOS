d6f6495c17da5be1de21e46f4d453af517c0def0
Yun <indl1670@gmail.com>
2023-03-29 16:16:11 +0900

    #S08P22E104-381 #commit feat:feature/front/recommend:[장소 추천 페이지 추가 기능 구현 - 추천 페이지 recoil state 설정]

--- front/src/recoil/states/RecommendPageState.ts
+++ front/src/recoil/states/RecommendPageState.ts
@@ -1,15 +1,5 @@
 import { atom } from "recoil";
 
-export const selectSidoState = atom({
-  key: "recoilSidoState",
-  default: { sidoCode: null, name: null },
-});
-
-export const recommendState = atom({
-  key: "recoilRecommendState",
-  default: { sido: null, gugun: null, categories: [], userSeq: null },
-});
-
 export const selectSido = atom({
   key: "selectSidoRec",
   default: { sidoCode: "", sidoName: "" },
@@ -17,5 +7,10 @@ export const selectSido = atom({
 
 export const selectGugun = atom({
   key: "selectGugunRec",
-  default: "",
+  default: { gugunCode: "", gugunName: "" },
+});
+
+export const selectCategory = atom({
+  key: "selectCategoryRec",
+  default: [{}],
 });
