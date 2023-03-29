4a9453099af719f982c096ff7b9602d0493ec15d
Yun <indl1670@gmail.com>
2023-03-29 11:45:22 +0900

    #S08P22E104-375 #commit feat:feature/front/search:[장소 검색 페이지 추가 기능 구현 - 레이아웃 & 추천 결과 페이지 수정]

--- front/src/layouts/SearchLayout.tsx
+++ front/src/layouts/SearchLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
