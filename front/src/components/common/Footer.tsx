b24bcb62875d2c4d26b77290636f2a11b3633ad3
dbwls387 <dbwls387@naver.com>
2023-03-29 15:07:28 +0900

    Fix: menu link

--- front/src/components/common/Footer.tsx
+++ front/src/components/common/Footer.tsx
@@ -4,52 +4,60 @@ import Cosmos from "../../assets/footer/light-cosmos.png";
 import CoupleIcon from "../../assets/footer/couple-icon.png";
 
 export default function Footer() {
-  return (
-    <div className="text-xl font-bold z-[9999999]">
-      <div className="w-screen max-w-[1200px] m-auto fixed bottom-0 z-100 left-0 inset-x-0 bg-white h-20 w-full flex">
-        <div className="menuItems m-auto w-full">
-          <div className="float-left w-1/5 flex justify-center">
-            <NavLink to="/">
-              <Icon
-                icon="healthicons:ui-user-profile"
-                color="#ff8e9e"
-                width="45"
-                height="45"
-              />
-            </NavLink>
-          </div>
-          <div className="float-left w-1/5 flex justify-center">
-            <NavLink to="/">
-              <Icon
-                icon="mdi:cards-heart"
-                color="#ff8e9e"
-                width="45"
-                height="45"
-              />
-            </NavLink>
-          </div>
-          <div className="float-left w-1/5 flex justify-center">
-            <NavLink to="/">
-              <img src={Cosmos} alt="logo" className="h-12 w-12" />
-            </NavLink>
-          </div>
-          <div className="float-left w-1/5 flex justify-center">
-            <NavLink to="/">
-              <img src={CoupleIcon} alt="logo" className="h-10 w-10" />
-            </NavLink>
-          </div>
-          <div className="float-left w-1/5 flex justify-center">
-            <NavLink to="/">
-              <Icon
-                icon="material-symbols:temp-preferences-custom"
-                color="#ff8e9e"
-                width="42"
-                height="42"
-              />
-            </NavLink>
-          </div>
+    return (
+        <div className="text-xl font-bold z-[9999999]">
+            <div className="w-screen max-w-[1200px] m-auto fixed bottom-0 z-100 left-0 inset-x-0 bg-white h-20 w-full flex">
+                <div className="menuItems m-auto w-full">
+                    <div className="float-left w-1/5 flex justify-center">
+                        <NavLink to="/mypage">
+                            <Icon
+                                icon="healthicons:ui-user-profile"
+                                color="#ff8e9e"
+                                width="45"
+                                height="45"
+                            />
+                        </NavLink>
+                    </div>
+                    <div className="float-left w-1/5 flex justify-center">
+                        <NavLink to="/wish">
+                            <Icon
+                                icon="mdi:cards-heart"
+                                color="#ff8e9e"
+                                width="45"
+                                height="45"
+                            />
+                        </NavLink>
+                    </div>
+                    <div className="float-left w-1/5 flex justify-center">
+                        <NavLink to="/">
+                            <img
+                                src={Cosmos}
+                                alt="logo"
+                                className="h-12 w-12"
+                            />
+                        </NavLink>
+                    </div>
+                    <div className="float-left w-1/5 flex justify-center">
+                        <NavLink to="/schedule/month">
+                            <img
+                                src={CoupleIcon}
+                                alt="logo"
+                                className="h-10 w-10"
+                            />
+                        </NavLink>
+                    </div>
+                    <div className="float-left w-1/5 flex justify-center">
+                        <NavLink to="/">
+                            <Icon
+                                icon="material-symbols:temp-preferences-custom"
+                                color="#ff8e9e"
+                                width="42"
+                                height="42"
+                            />
+                        </NavLink>
+                    </div>
+                </div>
+            </div>{" "}
         </div>
-      </div>{" "}
-    </div>
-  );
+    );
 }
