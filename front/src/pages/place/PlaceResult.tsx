f3bf181a3695a2db006329bf695a44dd07282afc
Yun <indl1670@gmail.com>
2023-03-30 10:27:16 +0900

    #S08P22E104-382 #commit feat:feature/front/recommend:[장소 추천 페이지 추가 기능 구현 - 추천 결과 페이지 기능 구현]

--- front/src/pages/place/PlaceResult.tsx
+++ front/src/pages/place/PlaceResult.tsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import {
   selectSido,
   selectGugun,
   selectCategory,
 } from "../../recoil/states/RecommendPageState";
+import { useQuery } from "react-query";
+import { getDateCourse } from "../../apis/api/place";
+import PlaceList from "../../components/place/search/lists/PlaceList";
+import TMap from "../../components/common/TMap";
 
 export default function PlaceResult() {
   const sido = useRecoilState(selectSido);
   const gugun = useRecoilState(selectGugun);
   const category = useRecoilState(selectCategory);
 
-  let formData = new FormData();
-  formData.append("sido", sido[0].sidoName);
-  formData.append("gugun", gugun[0].gugunName);
-  // formData.append("categories", category);
-  // formData.append("userSeq", 1);
-  return <div>결과</div>;
+  const tmp = {
+    sido: "서울특별시",
+    gugun: "강남구",
+    categories: category[0],
+    userSeq: 1,
+  };
+
+  const [item, setItem] = useState(JSON.stringify(tmp));
+
+  useEffect(() => {
+    setItem(JSON.stringify(tmp));
+  }, [sido[0].sidoName, gugun[0].gugunName, category[0]]);
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["getDateCourse", item],
+    queryFn: () => getDateCourse(item),
+  });
+
+  if (isLoading) return null;
+
+  return (
+    <div className="w-[90%] text-center">
+      <div className="mt-5">
+        <TMap />
+      </div>
+      {data === null || data === undefined ? null : (
+        <PlaceList data={data.data.places} />
+      )}
+    </div>
+  );
 }
