0c02a4620776a6e0b278d422ebe7d6afeaaf7cd3
강수나 <acrnm148@gmail.com>
2023-03-30 12:17:05 +0900

    fix : kakao Login

--- back/src/main/java/com/cosmos/back/oauth/provider/profile/KakaoProfile.java
+++ back/src/main/java/com/cosmos/back/oauth/provider/profile/KakaoProfile.java
@@ -19,21 +19,22 @@ public class KakaoProfile {
 
     @Data
     public class KakaoAccount {
-        public Profile profile;
         public Boolean profile_nickname_needs_agreement;
         public Boolean profile_image_needs_agreement;
+        public Profile profile;
         public Boolean has_email;
         public Boolean has_birthday;
-        public Boolean has_age_range;
         public Boolean email_needs_agreement;
-        public Boolean name_needs_agreement;
+        //연령, 생일 선택동의
+        public Boolean has_age_range;
         public Boolean age_range_needs_agreement;
         public Boolean birthday_needs_agreement;
         public Boolean phone_number_needs_agreement;
+
         public Boolean is_email_valid;
         public Boolean is_email_verified;
         public String email;
-        public String name;
+        //연령, 생일 선택동의
         public String age_range;
         public String birthday;
         public String phone_number;
