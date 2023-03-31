a6ef00a8e38f2a7482ac7f1ade3adf4e63d00156
Yun <indl1670@gmail.com>
2023-03-31 17:44:48 +0900

    #S08P22E101-385 #commit feat:feature/front/recommend:[장소 추천 페이지 추가 기능 구현 - TMap 연동]

--- front/src/components/common/TMapRecommend.tsx
+++ front/src/components/common/TMapRecommend.tsx
@@ -1,22 +1,65 @@
 import React, { useState, useEffect, useRef } from "react";
 import LightMarker from "../../assets/place/light-marker.png";
 import { useRecoilState } from "recoil";
-import { mapCenter, mapMarkers } from "../../recoil/states/RecommendPageState";
+import {
+  selectSido,
+  selectGugun,
+  selectCategory,
+  mapCenter,
+  mapMarkers,
+} from "../../recoil/states/RecommendPageState";
+import { useQuery } from "react-query";
+import { getDateCourse } from "../../apis/api/place";
+
 export default function TMapRecommend() {
+  const sido = useRecoilState(selectSido);
+  const gugun = useRecoilState(selectGugun);
+  const category = useRecoilState(selectCategory);
   const [mapCenterState, setMapCenterState] = useRecoilState(mapCenter);
   const [mapMarkersState, setMapMarkersState] = useRecoilState(mapMarkers);
-  console.log(mapMarkersState);
-  // const [position, setPosition] = useState(mapCenterState);
+
+  const tmp = {
+    sido: sido[0].sidoName,
+    gugun: gugun[0].gugunName,
+    categories: category[0],
+    userSeq: 1,
+  };
+
+  const [item, setItem] = useState(JSON.stringify(tmp));
   const [mapInstance, setMapInstance] = useState<Tmapv2.Map>();
+  const [markers, setMarkers] = useState<Tmapv2.Marker[]>();
   // 지도 div
   const mapRef = useRef<HTMLDivElement>(null);
 
+  const { data, isLoading } = useQuery({
+    queryKey: ["getDateCourse", item],
+    queryFn: () => getDateCourse(item),
+  });
+
   useEffect(() => {
-    // 좌표가 바뀔때마다 마커 재설정
-    mapInstance?.setCenter(
-      new window.Tmapv2.LatLng(mapCenterState.lat, mapCenterState.lng)
-    );
-  }, [mapCenterState]);
+    setItem(JSON.stringify(tmp));
+  }, [category[0]]);
+
+  useEffect(() => {
+    if (data !== null && data !== undefined) {
+      setMapCenterState({
+        lat: data.data.midLatitude,
+        lng: data.data.midLongitude,
+      });
+      const markers = [{}];
+      data.data.places.map((item: any) => {
+        markers.push({
+          lat: item.latitude,
+          lng: item.longitude,
+          placeId: item.placeId,
+          name: item.name,
+          type: item.type,
+        });
+      });
+      markers.splice(0, 1);
+      setMapMarkersState(markers);
+    }
+  }, [data]);
 
   useEffect(() => {
     const lat = mapCenterState.lat;
@@ -24,50 +67,81 @@ export default function TMapRecommend() {
     // 현재 좌표 위치로 지도 설정
     if (window.Tmapv2) {
       if (mapRef.current !== null) {
-        if (lat !== 0 && lng !== 0) {
-          const map = new window.Tmapv2.Map(mapRef.current, {
-            center: new window.Tmapv2.LatLng(lat, lng),
-            width: "100%",
-            height: "50vh",
-            zoom: 10,
-          });
+        const map = new window.Tmapv2.Map(mapRef.current, {
+          center: new window.Tmapv2.LatLng(lat, lng),
+          width: "100%",
+          height: "50vh",
+          zoom: 12,
+        });
 
-          const resultMarkerArr = [];
-          mapMarkersState.map((item: any, index: number) => {
-            if (index === 0) {
-              const marker_s = new window.Tmapv2.Marker({
-                position: new window.Tmapv2.LatLng(item.lat, item.lng),
-                icon: LightMarker,
-                map: map,
-                title: "이름",
-              });
-            }
-            if (item.lat !== null || item.lng !== null) {
-              const marker = new window.Tmapv2.Marker({
-                position: new window.Tmapv2.LatLng(item.lat, item.lng),
-                icon: LightMarker,
-                map: map,
-                title: "이름",
-              });
-            }
-
-            //   // 웹
-            //   marker.addListener("click", function () {
-            //     console.log("CLICK");
-            //   });
-            //   // 앱
-            //   marker.addListener("touchstart", function () {
-            //     console.log("터치!");
-            //   });
-          });
-
-          setMapInstance(map);
+        const newMarkers: ((prevState: never[]) => never[]) | Tmapv2.Marker[] =
+          [];
+        if (markers !== undefined) {
+          for (let i in markers) {
+            markers[i].setMap(null);
+          }
         }
+        mapMarkersState.map((item: any) => {
+          if (item.lat !== null || item.lng !== null) {
+            const marker = new window.Tmapv2.Marker({
+              position: new window.Tmapv2.LatLng(item.lat, item.lng),
+              icon: LightMarker,
+              map: map,
+              title: item.name,
+            });
+            newMarkers.push(marker);
+          }
+
+          //   // 웹
+          //   marker.addListener("click", function () {
+          //     console.log("CLICK");
+          //   });
+          //   // 앱
+          //   marker.addListener("touchstart", function () {
+          //     console.log("터치!");
+          //   });
+        });
+        setMarkers(newMarkers);
+        setMapInstance(map);
       }
     } else {
       console.error("TmapV2 API is not loaded");
     }
-  }, [mapCenterState]);
+  }, [mapRef.current]);
 
+  useEffect(() => {
+    if (markers !== undefined) {
+      for (let i in markers) {
+        markers[i].setMap(null);
+      }
+    }
+
+    const newMarkers: ((prevState: never[]) => never[]) | Tmapv2.Marker[] = [];
+    if (window.Tmapv2) {
+      if (mapInstance !== undefined) {
+        mapMarkersState.map((item: any) => {
+          if (item.lat !== null || item.lng !== null) {
+            const marker = new window.Tmapv2.Marker({
+              position: new window.Tmapv2.LatLng(item.lat, item.lng),
+              icon: LightMarker,
+              map: mapInstance,
+              title: item.name,
+            });
+            newMarkers.push(marker);
+          }
+        });
+      }
+    } else {
+      console.error("TmapV2 API is not loaded");
+    }
+
+    setMarkers(newMarkers);
+
+    mapInstance?.setCenter(
+      new window.Tmapv2.LatLng(mapCenterState.lat, mapCenterState.lng)
+    );
+  }, [mapMarkersState]);
+
+  if (isLoading || data === undefined) return null;
   return <div className="w-full h-[50vh]" id="TMAP" ref={mapRef}></div>;
 }
