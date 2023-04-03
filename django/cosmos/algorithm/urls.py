01ab8f2ef0e76595957d1a50ec77366e6c07fe27
dltkdcks456 <sangchan0825@gmail.com>
2023-04-03 10:14:26 +0900

    Add: django

---
+++ django/cosmos/algorithm/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('CF/algorithm/data', views.algorithm),
+    path('CF/algorithm/userSeq/<int:user_seq>/', views.userAlgorithm)
+]
