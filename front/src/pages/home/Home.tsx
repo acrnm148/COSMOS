e3cc9c5a4e900a7568badc0856175493788b1676
Yun <indl1670@gmail.com>
2023-04-04 12:01:53 +0900

    #S08P22E104-449 #commit feat:feature/front/darkMode:[메인 페이지 다크모드 설정]

--- front/src/pages/home/Home.tsx
+++ front/src/pages/home/Home.tsx
@@ -1,13 +1,14 @@
+import React, { useState } from "react";
 import RecommSlider from "../../components/home/RecommSlider";
 import MainMenuBtn from "../../components/home/MainMenuBtn";
 import MemoryComponent from "../../components/home/MemoryComponent";
 
 export default function Home() {
-    return (
-        <div className="pb-24 px-4 mt-8 w-full h-full">
-            <RecommSlider />
-            <MainMenuBtn />
-            <MemoryComponent />
-        </div>
-    );
+  return (
+    <div className="pb-24 px-4 mt-8 w-full h-screen">
+      <RecommSlider />
+      <MainMenuBtn />
+      <MemoryComponent />
+    </div>
+  );
 }
