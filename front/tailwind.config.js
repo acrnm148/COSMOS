e461de14b02383b1d49a4196dca2dd2f841c9b64
Yun <indl1670@gmail.com>
2023-04-04 11:59:41 +0900

    chore: tailwind 다크모드 설정

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
@@ -17,8 +18,8 @@ module.exports = {
         lightMain2: "#FFB5C0",
         lightMain3: "#FFDBE0",
         lightMain4: "#FFF0F2",
-        calendarGray : "#F4F4F4",
-        calendarDark : "#B9B9B9"
+        calendarGray: "#F4F4F4",
+        calendarDark: "#B9B9B9",
       },
       fontFamily: {
         baloo: ["Baloo 2", "cursive"],
