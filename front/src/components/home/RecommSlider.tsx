7b3336450b11423414e4a84ac8e344fa100629c7
Yun <indl1670@gmail.com>
2023-04-04 12:03:20 +0900

    #S08P22E104-449 #commit feat:feature/front/darkMode:[메인 페이지 다크모드 설정]

--- front/src/components/home/RecommSlider.tsx
+++ front/src/components/home/RecommSlider.tsx
@@ -5,6 +5,7 @@ import "../../css/slickTheme.css";
 import slider1 from "../../assets/home/slider1.png";
 import slider2 from "../../assets/home/slider2.png";
 import slider5 from "../../assets/home/slider5.png";
+import slider6 from "../../assets/home/slider6.png";
 
 export default function recommSlider() {
   var settings = {
@@ -19,6 +20,10 @@ export default function recommSlider() {
   };
 
   const cardContent: { title: string; imgSrc: string }[] = [
+    { title: "나를 위한 추천 코스", imgSrc: "slider6" },
+  ];
+
+  const cardContent2: { title: string; imgSrc: string }[] = [
     { title: "나를 위한 추천 코스", imgSrc: "slider1" },
     { title: "애인을 위한 추천 코스", imgSrc: "slider2" },
     { title: "우리 커플을 위한 추천 코스", imgSrc: "slider5" },
@@ -40,9 +45,11 @@ export default function recommSlider() {
 function Card(props: { content: string; imgSrc: string }) {
   return (
     <div>
-      <div className="list bg-lightMain4 w-full h-48 my-2 p-4 rounded-2xl relative">
-        <div className="content ml-3 mt-3 text-2xl font-bold z-40">{props.content}</div>
-        <div className="absolute top-[136px] ml-3 p-1 px-2 text-base font-light text-white bg-lightMain rounded-full">
+      <div className="list bg-lightMain4 w-full h-48 my-2 p-4 rounded-2xl relative dark:bg-darkMain3">
+        <div className="content ml-3 mt-3 text-2xl font-bold z-40 dark:text-white">
+          {props.content}
+        </div>
+        <div className="absolute top-[136px] ml-3 p-1 px-2 text-base font-light text-white bg-lightMain rounded-full dark:bg-darkMain">
           코스 보러 가기 ▶
         </div>
         <img
