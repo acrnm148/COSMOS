2e241cc9564725c219159e8a12ee9bc02f684ed0
dbwls387 <dbwls387@naver.com>
2023-04-05 11:50:23 +0900

    Fix: map scroll

--- front/src/components/common/ListCard.tsx
+++ front/src/components/common/ListCard.tsx
@@ -6,44 +6,45 @@ import { useRecoilState } from "recoil";
 import { darkMode } from "../../recoil/states/UserState";
 
 export default function ListCard({ children, height }: any | boolean) {
-  const isDark = useRecoilState(darkMode);
-  const [up, setUp] = useState(false);
-  const handleDropBtn = () => {
-    setUp((cur) => !cur);
-    const list = document.querySelector("#listBox") as HTMLElement;
-    if (up) {
-      list.style.marginTop = "0px";
-      height && (list.style.height = "70vh");
-    } else {
-      list.style.marginTop = "-50vh";
-      height && (list.style.height = "120vh");
-    }
-  };
+    const isDark = useRecoilState(darkMode);
+    const [up, setUp] = useState(false);
+    const handleDropBtn = () => {
+        setUp((cur) => !cur);
+        const list = document.querySelector("#listBox") as HTMLElement;
+        if (up) {
+            list.style.marginTop = "0px";
+            height && (list.style.height = "70vh");
+        } else {
+            list.style.marginTop = "-50vh";
+            height && (list.style.height = "120vh");
+        }
+    };
 
-  return (
-    <div
-      className={
-        "card mb-[80px] z-[100000] bg-white relative overflow-auto dark:bg-darkBackground" +
-        (height ? " h-[70vh]" : "")
-      }
-      id="listBox"
-    >
-      {up ? (
-        <ArrowDropDownIcon
-          fontSize="large"
-          color={isDark ? "secondary" : "disabled"}
-          onClick={handleDropBtn}
-          className="cursor-pointer"
-        />
-      ) : (
-        <ArrowDropUpIcon
-          fontSize="large"
-          color={isDark ? "secondary" : "disabled"}
-          onClick={handleDropBtn}
-          className="cursor-pointer"
-        />
-      )}
-      {children}
-    </div>
-  );
+    return (
+        <div
+            className={
+                "card mb-[80px] z-[100000] bg-white relative overflow-auto dark:bg-darkBackground" +
+                (height ? " h-[70vh]" : "") +
+                (!height ? " h-[62vh] overflow-y-scroll" : "")
+            }
+            id="listBox"
+        >
+            {up ? (
+                <ArrowDropDownIcon
+                    fontSize="large"
+                    color={isDark ? "secondary" : "disabled"}
+                    onClick={handleDropBtn}
+                    className="cursor-pointer"
+                />
+            ) : (
+                <ArrowDropUpIcon
+                    fontSize="large"
+                    color={isDark ? "secondary" : "disabled"}
+                    onClick={handleDropBtn}
+                    className="cursor-pointer"
+                />
+            )}
+            {children}
+        </div>
+    );
 }
