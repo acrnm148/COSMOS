2ca442e7921487135a2b933671878e305132065a
Yun <indl1670@gmail.com>
2023-04-07 00:07:15 +0900

    chore: conflict 해결

--- front/src/recoil/states/UserState.ts
+++ front/src/recoil/states/UserState.ts
@@ -11,22 +11,10 @@ export interface LUser {
 export const userState = atom<LUser>({
   key: "userState",
   default: {
-<<<<<<< HEAD
     seq: -1,
     isLoggedIn: false,
     acToken: "",
     coupleId: "0",
-=======
-    seq: 98,
-    isLoggedIn: true,
-    acToken:
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNzI3ODc5MzkyIiwiZXhwIjoxNjgwODEyMTgyLCJ1c2VySWQiOiIyNzI3ODc5MzkyIiwidXNlclNlcSI6MTl9.wvYGz4x64YuUE_fQAPB-L1UM1_yzPiWiNrbhWDNCtoBx6iRcNOtdquCRxx1oGM5Oo5emR1HrSH4_sfnRismCCw",
-    coupleId: "226927908",
-    // seq: -1,
-    // isLoggedIn: false,
-    // acToken: "",
-    // coupleId: "0",
->>>>>>> c7337cf393bb212864516e3eda571f9e67ce4b4e
   },
   // effects_UNSTABLE: [persistAtom],
 });
